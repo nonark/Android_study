fa2ad779403c500a7a91b0bc1f15d653d279fa0c
Kim Hyun Bong <nonark@naver.com>
2017-04-07 11:34:41 +0900

    Activity 에서 검색 시, 공통으로 사용하는 AsyncTask를 별도 클래스로 분할/적

--- app/src/main/java/search/android/aos_search/DetailActivity.java
+++ app/src/main/java/search/android/aos_search/DetailActivity.java
@@ -1,22 +1,19 @@
 package search.android.aos_search;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import search.android.adapter.SummaryPageAdapter;
 import search.android.customview.StatusBar;
-import search.android.tools.WikiPageFinder;
+import search.android.task.WikiPageSearchTask;
 import search.android.vo.SummaryPage;
 
 /**
@@ -28,33 +25,12 @@ public class DetailActivity extends Activity {
     StatusBar statusBar;
     RecyclerView wikiPagesView;
     SummaryPageAdapter adapter;
-    ProgressDialog dialog;
+    WikiPageSearchTask task;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
-
-        dialog = new ProgressDialog(this);
-        dialog.setMessage("Loading...");
-
-        statusBar = (StatusBar) findViewById(R.id.statusBar);
-        statusBar.setOnBackButtonClickedListener(new StatusBar.OnStatusBarClickedListener() {
-            @Override
-            public void onStatusButtonClicked() {
-                finish();
-            }
-        });
-
-        statusBar.setOnCloseButtonClickedListener(new StatusBar.OnStatusBarClickedListener() {
-            @Override
-            public void onStatusButtonClicked() {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                startActivity(intent);
-            }
-        });
 
         wikiPagesView = (RecyclerView) findViewById(R.id.wikiPages);
 
@@ -82,57 +58,30 @@ public class DetailActivity extends Activity {
         wikiPagesView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         wikiPagesView.setItemAnimator(new DefaultItemAnimator());
 
+        statusBar = (StatusBar) findViewById(R.id.statusBar);
+        statusBar.setOnBackButtonClickedListener(new StatusBar.OnStatusBarClickedListener() {
+            @Override
+            public void onStatusButtonClicked() {
+                finish();
+            }
+        });
+
+        statusBar.setOnCloseButtonClickedListener(new StatusBar.OnStatusBarClickedListener() {
+            @Override
+            public void onStatusButtonClicked() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+            }
+        });
+
         Intent intent = getIntent();
         if (intent != null) {
             String searchText = intent.getStringExtra("Search");
             statusBar.setTitle(searchText);
-            WikiPageSearchTask task = new WikiPageSearchTask(adapter);
+            task = new WikiPageSearchTask(this, adapter);
             task.execute(searchText);
-        }
-    }
-
-    private class WikiPageSearchTask extends AsyncTask<String, Void, Void> {
-
-        private SummaryPageAdapter summaryPageAdapter;
-        private List<SummaryPage> wikiPages;
-
-        public WikiPageSearchTask(SummaryPageAdapter summaryPageAdapter) {
-            super();
-            this.summaryPageAdapter = summaryPageAdapter;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            dialog.show();
-            wikiPages = null;
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-            wikiPages = WikiPageFinder.findRelatedPages(params[0]);
-
-            if(wikiPages == null) {
-                wikiPages = new ArrayList<>();
-            }
-
-            SummaryPage summaryPage = WikiPageFinder.findSummaryPage(params[0]);
-
-            if(summaryPage != null) {
-                wikiPages.add(0, summaryPage);
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            summaryPageAdapter.setWikiPages(wikiPages);
-            summaryPageAdapter.notifyDataSetChanged();
-            dialog.cancel();
-            if(wikiPages.size() == 0) {
-                Toast.makeText(getApplicationContext(), "검색어를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show();
-            }
         }
     }
 }
--- app/src/main/java/search/android/aos_search/MainActivity.java
+++ app/src/main/java/search/android/aos_search/MainActivity.java
@@ -1,21 +1,18 @@
 package search.android.aos_search;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import search.android.adapter.SummaryPageAdapter;
 import search.android.customview.SearchBar;
-import search.android.tools.WikiPageFinder;
+import search.android.task.WikiPageSearchTask;
 import search.android.vo.SummaryPage;
 
 public class MainActivity extends Activity {
@@ -23,32 +20,20 @@ public class MainActivity extends Activity {
     SearchBar searchBar;
     RecyclerView wikiPagesView;
     SummaryPageAdapter adapter;
-    ProgressDialog dialog;
+    WikiPageSearchTask task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dialog = new ProgressDialog(this);
-        dialog.setMessage("Loading...");
-
-        searchBar = (SearchBar) findViewById(R.id.searchBar);
-        searchBar.setOnSearchBarClickedListener(new SearchBar.OnSearchBarClickedListener() {
-            @Override
-            public void onSearchButtonClicked(final String searchText) {
-
-                WikiPageSearchTask task = new WikiPageSearchTask(adapter);
-                task.execute(searchText);
-
-            }
-        });
 
         wikiPagesView = (RecyclerView) findViewById(R.id.wikiPages);
 
         List<SummaryPage> wikiPages = new ArrayList<>();
 
         adapter = new SummaryPageAdapter(wikiPages, R.layout.search_item_header ,R.layout.search_item);
+
         adapter.setRelatedListner(new SummaryPageAdapter.OnRecyclerViewItemClickedListener() {
             @Override
             public void onItemClicked(String searchText) {
@@ -72,52 +57,14 @@ public class MainActivity extends Activity {
         wikiPagesView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         wikiPagesView.setItemAnimator(new DefaultItemAnimator());
 
+        searchBar = (SearchBar) findViewById(R.id.searchBar);
+        task = new WikiPageSearchTask(this, adapter);
+        searchBar.setOnSearchBarClickedListener(new SearchBar.OnSearchBarClickedListener() {
+            @Override
+            public void onSearchButtonClicked(final String searchText) {
+                task.execute(searchText);
 
-    }
-
-    private class WikiPageSearchTask extends AsyncTask<String, Void, Void> {
-
-        private SummaryPageAdapter summaryPageAdapter;
-        private List<SummaryPage> wikiPages;
-
-        public WikiPageSearchTask(SummaryPageAdapter summaryPageAdapter) {
-            super();
-            this.summaryPageAdapter = summaryPageAdapter;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            dialog.show();
-            wikiPages = null;
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-            wikiPages = WikiPageFinder.findRelatedPages(params[0]);
-
-            if(wikiPages == null) {
-                wikiPages = new ArrayList<>();
             }
-
-            SummaryPage summaryPage = WikiPageFinder.findSummaryPage(params[0]);
-
-            if(summaryPage != null) {
-                wikiPages.add(0, summaryPage);
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            summaryPageAdapter.setWikiPages(wikiPages);
-            summaryPageAdapter.notifyDataSetChanged();
-            dialog.cancel();
-            if(wikiPages.size() == 0) {
-                Toast.makeText(getApplicationContext(), "검색어를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show();
-            }
-        }
+        });
     }
 }
